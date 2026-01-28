d40cceac914926f9bb7e58dd1928e454124adc70
perlancar <perlancar@gmail.com>
2026-01-28 10:21:19 +0700

    [bugfix] Bug regex to check base 32 digits

--- lib/App/CrockfordBase32Utils.pm
+++ lib/App/CrockfordBase32Utils.pm
@@ -115,8 +115,8 @@ sub cfbase32_decode {
     [200, "OK", Convert::Base32::Crockford::decode_base32($str)];
 }
 
-my $cfbase32_digit_re    = qr/[0-9A-HJ-NP-TV-Z]/;
-my $cfbase32_nondigit_re = qr/[^0-9A-HJ-NP-TV-Z]/;
+my $cfbase32_digit_re    = qr/[0-9A-HJKMNP-TV-Z]/;
+my $cfbase32_nondigit_re = qr/[^0-9A-HJKMNP-TV-Z]/;
 
 my @cfbase32_digits = qw(0 1 2 3 4 5 6 7 8 9
                          A B C D E F G H J K
